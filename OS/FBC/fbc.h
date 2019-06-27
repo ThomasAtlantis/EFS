6436b5a798203a3e2fdf181f58e29c5f1d91c66d
ThomasAtlantis <1138670081@qq.com>
2019-06-27 11:11:17 +0800

    为VHD和FBC添加了详细的接口注释

--- OS/FBC/fbc.h
+++ OS/FBC/fbc.h
@@ -19,6 +19,7 @@
 
 class FBController { // 使用成组链接法管理空闲块
 private:
+    // 超级块的结构定义
     #pragma pack (1)
     typedef struct {
         bid_t freeCount; // 空闲块总数量
@@ -26,78 +27,137 @@ private:
         char padding[_SUPBLOCK_PADDING_SIZE];
     } SuperBlock;
     #pragma pack ()
+
     const bid_t _maxBlockID; // 受管理的最大块号
     const bid_t _minBlockID; // 受管理的最小块号
     SuperBlock * _superBlock; // 内存超级块缓冲区
     VHDController& _vhdc; // 通过引用vhdc调用VHD接口
+
+    /**
+     * 获取新块并初始化
+     * @return 新块的指针
+     */
     char * newBlock() {
         auto * block = new char [_BLOCK_SIZE];
-        memset(block, 0, _BLOCK_SIZE);
+        memset(block, 0, _BLOCK_SIZE); // 清0
         return block;
     }
+
+    /**
+     * 获取新超级块并初始化
+     * @return 超级块指针
+     */
     SuperBlock * newSuperBlock() {
         auto * sb = new SuperBlock;
-        sb->freeCount = 0;
-        for (auto & freeBlockID: sb->freeStack)
+        sb->freeCount = 0; // 栈顶指针归0
+        for (auto & freeBlockID: sb->freeStack) // 栈内数据清0
             freeBlockID = 0;
         memset(sb->padding, 0, _SUPBLOCK_PADDING_SIZE);
         return sb;
     }
+
+    /**
+     * 空闲块栈压栈
+     * @param sb 超级块
+     * @param blockID 空闲块号
+     */
     void push(SuperBlock * sb, bid_t blockID) {
         sb->freeStack[sb->freeCount] = blockID;
         sb->freeCount ++;
     }
+
+    /**
+     * 空闲块栈弹栈
+     * @param sb 超级块
+     * @return 空闲块号
+     */
     bid_t pop(SuperBlock * sb) {
         sb->freeCount --;
         return sb->freeStack[sb->freeCount];
     }
 public:
-    FBController(VHDController& vhdc, bid_t minBlockID, bid_t maxBlockID):
-        _vhdc(vhdc), _minBlockID(minBlockID), _maxBlockID(maxBlockID), _superBlock(nullptr) {
+    /**
+     * 构造函数，尝试从硬盘读取超级块
+     * @param vhdc 虚拟硬盘管理器
+     * @param minBlockID 管理的最小块号
+     * @param maxBlockID 管理的最大块号
+     */
+    FBController(VHDController& vhdc,
+        bid_t minBlockID,
+        bid_t maxBlockID):
+        _vhdc(vhdc),
+        _minBlockID(minBlockID),
+        _maxBlockID(maxBlockID) {
         _superBlock = newSuperBlock();
         loadSuperBlock();
     }
 
+    /**
+     * 格式化硬盘
+     * @return 操作成功与否
+     */
     bool formatting() {
         delete _superBlock;
         _superBlock = newSuperBlock();
-        push(_superBlock, _minBlockID);
+        push(_superBlock, _minBlockID); // 保留最小块号作尾结束指针，同时也是储存超级块的位置
         for (bid_t blockID = _minBlockID + 1; blockID <= _maxBlockID; ++ blockID)
-            recycle(blockID);
-        saveSuperBlock();
+            recycle(blockID); // 依次回收剩下的所有块
+        saveSuperBlock(); // 只要超级块有变化，刷到硬盘
         return true;
     }
 
-    bool loadSuperBlock() { // 加载超级块
-        return _vhdc.readBlock((char *) _superBlock, _minBlockID);
+    /**
+     * 加载超级块
+     * @return 操作成功与否
+     */
+    bool loadSuperBlock() {
+        return _vhdc.readBlock((char *) _superBlock, _minBlockID); // 超级块存在最小块号处
     }
 
-    bool saveSuperBlock() { // 保存超级块到VHD
-        return _vhdc.writeBlock((char *) _superBlock, _minBlockID);
+    /**
+     * 保存超级块到VHD
+     * @return 操作成功与否
+     */
+    bool saveSuperBlock() {
+        return _vhdc.writeBlock((char *) _superBlock, _minBlockID); // 超级块存在最小块号处
     }
 
-    bool recycle(bid_t blockID) { // 回收blockID指定的块
-        if (_superBlock->freeCount < _BLOCK_GROUP_SIZE) {
-            push(_superBlock, blockID);
-        } else {
-            _vhdc.writeBlock((char *) _superBlock, blockID);
-            delete _superBlock;
+    /**
+     * 回收blockID指定的块
+     * @param blockID 待回收的块号
+     * @return 操作成功与否
+     */
+    bool recycle(bid_t blockID) {
+        if (_superBlock->freeCount < _BLOCK_GROUP_SIZE) { // 如果栈未满
+            push(_superBlock, blockID); // 压栈
+        } else { // 如果栈满
+            _vhdc.writeBlock((char *) _superBlock, blockID); // 超级块写入空闲块，成为下级组长块
+            delete _superBlock; // 删除超级块，重新初始化为空
             _superBlock = newSuperBlock();
-            push(_superBlock, blockID);
+            push(_superBlock, blockID); // 将下级组长块压栈
         }
         return true;
     }
 
-    bool distribute(bid_t & blockID) { // 分配一个空闲块，并将块号放到blockID中，若失败则返回false
-        if (_superBlock->freeCount > 1) {
-            blockID = pop(_superBlock);
+    /**
+     * 分配一个空闲块
+     * @param blockID 存放分配的块号返回值
+     * @return 操作成功与否
+     */
+    bool distribute(bid_t & blockID) {
+        if (_superBlock->freeCount > 1) { // 如果栈中空闲块数大于1
+            blockID = pop(_superBlock); // 弹栈
         } else if (_superBlock->freeStack[0] != _minBlockID) {
-            blockID = pop(_superBlock);
-            _vhdc.readBlock((char *) _superBlock, blockID);
-        } else return false;
+            blockID = pop(_superBlock); // 弹栈，释放下一级组长块
+            _vhdc.readBlock((char *) _superBlock, blockID); // 把组长块写入到内存超级块缓冲区
+        } else return false; // 如果栈剩余的1块为0号，说明磁盘已满
         return true;
     }
 
+    /**
+     * 超级块公有操作接口
+     * @return 超级块的引用
+     */
     SuperBlock * superBlock() {
         return _superBlock;
     }
--- OS/VHD/vhd.h
+++ OS/VHD/vhd.h
@@ -12,28 +12,50 @@
 #include "..\..\utilities.h"
 #include "..\params.h"
 
-typedef size_t bid_t;
+typedef size_t bid_t; // u_int64(unsigned long long int)
 
 class VHDController {
 private:
     string _fileName; // 虚拟磁盘的文件路径
     std::fstream _fileStream; // 虚拟磁盘文件流
-    size_t _blockSize;
+    size_t _blockSize; // 块大小
 public:
+    /**
+     * 构造函数，打开文件二进制读写流
+     * @param fileName 虚拟磁盘的文件**相对**路径
+     * @param blockSize 块大小，默认为宏定义值
+     */
     explicit VHDController(string fileName, size_t blockSize = _BLOCK_SIZE):
             _fileName(std::move(fileName)), _blockSize(blockSize) {
         _fileStream.open(_fileName.c_str(), std::ios::in | std::ios::out| std::ios::binary);
+        // 单独std::ios::out 从头覆盖，文件大小只有最后一次写的大小
+        // 加入std::ios::append 文件大小会不断增大
+        // std::ios::in| std::ios::out 可以做到修改文件某一块
     }
     ~VHDController() {
         _fileStream.close();
     }
+    /**
+     * 读磁盘块
+     * @param buffer 存储读出的数据的缓冲区
+     * @param blockID 要读的块号
+     * @param len 连续读的块数
+     * @return 是否成功
+     */
     bool readBlock(char * buffer, bid_t blockID, int len = 1) { // 按块读
-        _fileStream.seekg(blockID * _blockSize, std::ios::beg);
+        _fileStream.seekg(blockID * _blockSize, std::ios::beg); // 移动文件流指针到指定位置
         _fileStream.read(buffer, _blockSize * len);
         return true;
     }
+    /**
+     * 写磁盘块
+     * @param buffer 存储要写的数据的缓冲区
+     * @param blockID 要写的块号
+     * @param len 连续写的块数
+     * @return 是否成功
+     */
     bool writeBlock(char * buffer, bid_t blockID, int len = 1) { // 按块写
-        _fileStream.seekp(blockID * _blockSize, std::ios::beg);
+        _fileStream.seekp(blockID * _blockSize, std::ios::beg); // 移动文件流指针到指定位置
         _fileStream.write(buffer, _blockSize * len);
         return !!_fileStream;
     }
